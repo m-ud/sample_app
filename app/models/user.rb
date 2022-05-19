f5f416af5d75072676848be462d975aa1f8f9839
M.U <b2107mu@aiit.ac.jp>
2022-05-19 14:34:56 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   before_save { email.downcase! }
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -11,5 +12,13 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  #試作feedの定義
+  #完全な実装はユーザーをフォローするを参照
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
+  private
   
 end
